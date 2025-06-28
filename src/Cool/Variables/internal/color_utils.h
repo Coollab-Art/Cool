05d985d5578b2b6b70baffff5cdc6ca19879c1e6
Jules Fouchy <jules.fouchy@ntymail.com>
2025-06-28 14:58:11 +0200

    🤏 In UI, colors are not shown as integers [0, 255] instead of float [0, 1]

--- src/Cool/Variables/internal/color_utils.h
+++ src/Cool/Variables/internal/color_utils.h
@@ -5,8 +5,7 @@ namespace Cool::internal {
 
 inline auto color_imgui_flags(bool is_hdr) -> ImGuiColorEditFlags
 {
-    return ImGuiColorEditFlags_Float
-           | (is_hdr ? ImGuiColorEditFlags_HDR : 0)
+    return (is_hdr ? ImGuiColorEditFlags_HDR : 0)
            | ImGuiColorEditFlags_PickerHueWheel
            | ImGuiColorEditFlags_AlphaBar;
 }
@@ -14,14 +13,14 @@ inline auto color_imgui_flags(bool is_hdr) -> ImGuiColorEditFlags
 inline auto color_metadata_widget(bool& is_hdr) -> bool
 {
     const bool b = ImGuiExtras::toggle("HDR", &is_hdr);
-    ImGuiExtras::help_marker("Allows the RGB values to go outside of the [0, 1] range.");
+    ImGuiExtras::help_marker("Allows the RGB values to go outside of the [0, 255] range.");
     return b;
 }
 
 inline auto color_and_alpha_metadata_widget(bool& is_hdr) -> bool
 {
     const bool b = ImGuiExtras::toggle("HDR", &is_hdr);
-    ImGuiExtras::help_marker("Allows the RGBA values to go outside of the [0, 1] range.");
+    ImGuiExtras::help_marker("Allows the RGBA values to go outside of the [0, 255] range.");
     return b;
 }
 
